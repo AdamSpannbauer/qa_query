91734e49a187a4f6d9b31d2a8d5c28167f12f5ba
Adam Spannbauer <u775749@emn.com>
2019-09-05 16:35:59 -0400

    playing with ner preprocessing

--- feature_pipeline.py
+++ feature_pipeline.py
@@ -2,10 +2,12 @@ import json
 import glob
 import pickle
 import pandas as pd
+from tqdm import tqdm
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
 from sklearn.base import TransformerMixin
+import nltk
 from annoy import AnnoyIndex
 
 ARTICLE_DF_PATH = 'data/articles.csv'
@@ -15,8 +17,26 @@ ANNOY_PATH = 'data/annoy_idx.ann'
 # From Annoy docs:
 #     Works better if you don't have too many dimensions (like <100)
 #     but seems to perform surprisingly well even up to 1,000 dimensions
-N_DIM = 512
+N_DIM = 1000
 N_TREES = 50
+
+
+def nltk_ner_extract(text, keep_labels=None, join=True):
+    if keep_labels is None:
+        keep_labels = ['ORGANIZATION', 'LOCATION', 'FACILITY', 'GPE']
+
+    chunks = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(text)))
+    ne_list = []
+    for chunk in chunks:
+        if hasattr(chunk, 'label'):
+            if chunk.label() in keep_labels:
+                full_ne = ' '.join(c[0] for c in chunk)
+                ne_list.append(full_ne)
+
+    if join:
+        return ' '.join(ne_list)
+    else:
+        return ne_list
 
 
 def read_json_articles_to_df(json_glob='data/articles/*.json'):
@@ -27,12 +47,14 @@ def read_json_articles_to_df(json_glob='data/articles/*.json'):
 
     articles_df = pd.DataFrame(article_list)
 
-    # TODO: don't duplicate full_text like this
-    articles_df['full_text'] = articles_df['text'].apply(lambda x: '\n\n'.join(x))
-    articles_df = articles_df.explode('text')
-    articles_df = articles_df.drop_duplicates().dropna()
+    full_df = articles_df.copy()
+    full_df['text'] = full_df['text'].apply(lambda x: '\n\n'.join(x))
+    full_df = full_df.drop_duplicates().dropna()
 
-    return articles_df
+    section_df = articles_df.explode('text')
+    section_df = section_df.drop_duplicates().dropna()
+
+    return section_df, full_df
 
 
 # noinspection PyUnusedLocal
@@ -46,15 +68,22 @@ class DenseTransformer(TransformerMixin):
 
 
 if __name__ == '__main__':
-    article_df = read_json_articles_to_df()
+    section_text_df, full_text_df = read_json_articles_to_df()
+    ne_text = []
+    for t in tqdm(full_text_df['text']):
+        ne_text.append(nltk_ner_extract(t))
+    full_text_df['ne_text'] = ne_text
+
+    section_text_df.to_csv(ARTICLE_DF_PATH, index=False)
+    full_text_df.to_csv('data/ne_full_text_df.csv', index=False)
 
     preprocessing_pipeline = Pipeline([
-        ('tfidf', TfidfVectorizer()),
+        ('tfidf', TfidfVectorizer(stop_words='english')),
         ('to_dense', DenseTransformer()),
         ('pca', PCA(n_components=N_DIM, random_state=42)),
     ], verbose=True)
 
-    features = preprocessing_pipeline.fit_transform(article_df['text'])
+    features = preprocessing_pipeline.fit_transform(section_text_df['text'])
     annoy_idx = AnnoyIndex(N_DIM, 'euclidean')
 
     for i, record in enumerate(features):
@@ -63,6 +92,5 @@ if __name__ == '__main__':
     annoy_idx.build(N_TREES)
 
     annoy_idx.save(ANNOY_PATH)
-    article_df.to_csv(ARTICLE_DF_PATH, index=False)
     with open(PREPROCESSING_PIPELINE_PATH, 'wb') as file:
         pickle.dump(preprocessing_pipeline, file, protocol=pickle.HIGHEST_PROTOCOL)
