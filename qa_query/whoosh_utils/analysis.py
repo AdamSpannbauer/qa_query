a548276684bd938d8b7ff9db792b6545a5889983
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-09-29 08:36:59 -0400

    update handling of ne types

--- qa_query/whoosh_utils/analysis.py
+++ qa_query/whoosh_utils/analysis.py
@@ -6,19 +6,21 @@ from whoosh.analysis.tokenizers import default_pattern
 NLTK_STOPWORDS = set(nltk.corpus.stopwords.words('english'))
 QUESTION_STOPWORDS = {'who', 'what', 'where', 'when', 'why', 'how'}
 QA_STOPWORDS = frozenset(QUESTION_STOPWORDS | NLTK_STOPWORDS)
-KEEP_NE_TYPES = frozenset(['ORGANIZATION', 'LOCATION', 'FACILITY', 'GPE'])
+QA_NE_TYPES = frozenset(['ORGANIZATION', 'LOCATION', 'FACILITY', 'GPE'])
+ALL_NE_TYPES = frozenset(['ORGANIZATION', 'PERSON', 'LOCATION', 'DATE', 'TIME',
+                          'MONEY', 'PERCENT', 'FACILITY', 'GPE'])
 
 
-def ner_extract(text, ne_types=KEEP_NE_TYPES):
-    # TODO: if ner seems to be bottleneck in accuracy investigate non-nltk options
+def ner_extract(text, ne_types=QA_NE_TYPES):
     """Remove non named entities from a string
-
-    Currently being processed with nltk.ne_chunk()
 
     :param text: str to remove non named entities from
     :param ne_types: list/set of named entities to keep
     :return: text with non named entities removed
     """
+    if ne_types is None:
+        ne_types = ALL_NE_TYPES
+
     chunks = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(text)))
     ne_list = []
     for chunk in chunks:
@@ -30,21 +32,24 @@ def ner_extract(text, ne_types=KEEP_NE_TYPES):
     return ' '.join(ne_list)
 
 
-class NERTokenizer(RegexTokenizer, object):
-    """Named Entity centric version of whoosh_utils.analysis.tokenizers.RegexTokenizer"""
-    def __call__(self, text, ne_types=KEEP_NE_TYPES, **kwargs):
-        text = ner_extract(text=text, ne_types=ne_types)
+class NERTokenizer(RegexTokenizer):
+    """Named Entity centric version of RegexTokenizer"""
+    def __init__(self, ne_types=QA_NE_TYPES, expression=default_pattern, gaps=False):
+        self.ne_types = ne_types
+        super().__init__(expression=expression, gaps=gaps)
+
+    def __call__(self, text, **kwargs):
+        text = ner_extract(text=text, ne_types=self.ne_types)
         return super().__call__(text, **kwargs)
 
 
 # Conforming to camelCase convention used for analyzer functions in whoosh_utils
 # noinspection PyPep8Naming
-def NERAnalyzer(expression=default_pattern, stoplist=QA_STOPWORDS,
+def NERAnalyzer(ne_types=QA_NE_TYPES, expression=default_pattern, stoplist=QA_STOPWORDS,
                 minsize=2, maxsize=None, gaps=False):
-    """Named Entity centric version of whoosh_utils.analysis.tokenizers.RegexTokenizer
+    """Named Entity centric version of StandardAnalyzer
 
-    Removes all text not classified as part of a Named Entity before applying StandardAnalyzer
-
+    :param ne_types: list/set of named entities to keep
     :param expression: The regular expression pattern to use to extract tokens.
     :param stoplist: A list of stop words. Set this to None to disable
         the stop word filter.
@@ -54,21 +59,21 @@ def NERAnalyzer(expression=default_pattern, stoplist=QA_STOPWORDS,
         than matching on the expression.
     :return: analyzer to be used with a whoosh_utils index
     """
-    chain = NERTokenizer(expression, gaps)
+    chain = NERTokenizer(ne_types, expression, gaps)
     chain |= LowercaseFilter()
     chain |= StopFilter(stoplist=stoplist, minsize=minsize, maxsize=maxsize)
-    chain |= StemFilter()
 
     return chain
 
 
 # Conforming to camelCase convention used for analyzer functions in whoosh_utils
 # noinspection PyPep8Naming
-def QAAnalyzer(ner_tokenize=False, expression=default_pattern,
+def QAAnalyzer(ner_tokenize=False, ne_types=QA_NE_TYPES, expression=default_pattern,
                stoplist=QA_STOPWORDS, minsize=2, maxsize=None, gaps=False):
     """Custom whoosh_utils analyzer for processing Named Entities and using custom stoplist
 
     :param ner_tokenize: Should NERAnalyzer be used instead of StandardAnalyzer
+    :param ne_types: list/set of named entities to keep
     :param expression: The regular expression pattern to use to extract tokens.
     :param stoplist: A list of stop words. Set this to None to disable
         the stop word filter.
@@ -81,4 +86,4 @@ def QAAnalyzer(ner_tokenize=False, expression=default_pattern,
     if not ner_tokenize:
         return StemmingAnalyzer(expression, stoplist, minsize, maxsize, gaps)
     else:
-        return NERAnalyzer(expression, stoplist, minsize, maxsize, gaps)
+        return NERAnalyzer(ne_types, expression, stoplist, minsize, maxsize, gaps)
