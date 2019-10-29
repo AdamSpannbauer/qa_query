c2b572f513b3a9bf8afedff4cec3caf7865fcfa0
James Robinson <james@bermuda.earth>
2019-10-29 14:16:58 -0700

    Added a work on mac script

---
+++ sanity-check.py
@@ -0,0 +1,7 @@
+from deeppavlov import build_model, configs
+
+document = ['The rain in Spain falls mainly on the plain.']
+question = ['Where does the rain in Spain fall?']
+
+model = build_model(configs.squad.squad, download=True)
+print(model(document, question))
--- work-on-mac.sh
+++ work-on-mac.sh
@@ -8,12 +8,21 @@ fi
 
 echo "Correct version of Python, 3.6.5, detacted."
 
-echo Installing requirements
+read -n 1 -p "Press any key to run 'pip install -r requirements.txt' and install the requirements" dummy
 pip install -r requirements.txt 
 
 
-echo Installing Bert
+read -n 1 -p "Press any key to run 'python -m deeppavlov install squad_bert' and install berts" dummy
 python -m deeppavlov install squad_bert
 
+echo Now you need to install nltk. Run the command:
+echo
+echo python nltk_download.py
+echo
+echo I had to run over 10 times due to timeouts. Once crashed you may need to use ctrl-c to exit the python
+echo
+read -n 1 -p "Press any once has completed successfully:" dummy
 
+read -n 1 -p "Press any key to run the sanity check which will also download the library" dummy
+python sanity-check.py
 
